5c08bf97a36003f71a57988e4c92c384f2d73d11
alfilister <castillo.c.jm@gmail.com>
2022-04-28 23:31:54 -0500

    Paginado Component converted to ClassComponent

--- client/src/components/Paginado.jsx
+++ client/src/components/Paginado.jsx
@@ -1,24 +1,22 @@
 import style from "../styles/Paginado.module.scss"
-import React from "react"
+import React, { Component } from "react"
 
-export default function Paginado({
-  videogamesPerPage,
-  allVideogames,
-  paginado,
-}) {
-  const pageNumbers = []
-
-  for (let i = 1; i <= Math.ceil(allVideogames / videogamesPerPage); i++) {
-    pageNumbers.push(i)
+export default class Paginado extends Component {
+  render() {
+    const { videogamesPerPage, allVideogames, paginado } = this.props
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(allVideogames / videogamesPerPage); i++) {
+      pageNumbers.push(i)
+    }
+    return (
+      <nav className={style.pagNav}>
+        {pageNumbers &&
+          pageNumbers.map((number) => (
+            <button key={number} onClick={() => paginado(number)}>
+              {number}
+            </button>
+          ))}
+      </nav>
+    )
   }
-  return (
-    <nav className={style.pagNav}>
-      {pageNumbers &&
-        pageNumbers.map((number) => (
-          <button key={number} onClick={() => paginado(number)}>
-            {number}
-          </button>
-        ))}
-    </nav>
-  )
 }
